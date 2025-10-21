cd7f39049dccd907b796d2fa5a5b207b276d5175
Ashley4-4 <alorenzo@uoguelph.ca>
2025-10-20 20:30:50 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,26 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+
+#Function to get and validate user input
+def get_num_terms():
+  while true:
+    print("How many terms od the fibbonacci sequence do you want?")
+    num = input()
+    if num > 0:
+      return num
+    else 
+      print("Error please input a positive number")
+
+# Function to make fibbonacci sequence
+def fibonacci_sequence(n):
+  sequence = []
+  a, b = (0, 1)
+  for _ in range(n):
+    sequence.append(a)
+    a, b = b, a + b
+  return sequence
+
+#function to print the sequence
+def print_sequence(seq):
+  print("\nFibbonacci Sequence: seq")
